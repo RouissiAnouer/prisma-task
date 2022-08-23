02948c5abb850225696e98beca7f072731895753
Anouer Rouissi <anouerrouissi@yahoo.fr>
2022-08-24 00:54:40 +0200

    additional fix

--- prisma-task/src/main/java/com/onpier/task/messaging/MessagingConfiguration.java
+++ prisma-task/src/main/java/com/onpier/task/messaging/MessagingConfiguration.java
@@ -2,6 +2,11 @@ package com.onpier.task.messaging;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +27,13 @@ public class MessagingConfiguration {
     @Value("${spring.rabbitmq.queue}")
     private String queueName;
 
+    @Value("${spring.rabbitmq.host}")
+    String host;
+    @Value("${spring.rabbitmq.username}")
+    String username;
+    @Value("${spring.rabbitmq.password}")
+    String password;
+
     @Bean
     DirectExchange exchange() {
         return new DirectExchange(exchange);
@@ -35,6 +47,24 @@ public class MessagingConfiguration {
     @Bean
     public List<Binding> binding() {
         return Arrays.asList(BindingBuilder.bind(queue()).to(exchange()).with(routingkey));
+    }
+
+    @Bean
+    CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
+    }
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 
 
